e94470fc1d2ef47d0577b0af4809a3a0701249f4
chengjian <1210786785@qq.com>
2019-06-21 00:01:29 +0800

    自定义Wordcount

---
+++ src/main/java/com/cheng/exercise/WordCountFromWindow.java
@@ -0,0 +1,33 @@
+package com.cheng.exercise;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+public class WordCountFromWindow {
+
+    public static void main(String[] args) throws Exception {
+
+        //创建 execution environment
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> textStream = environment.socketTextStream("localhost", 9999, "\t");
+        DataStream<Tuple2<String, Integer>> windowCounts = textStream.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                for (String word : value.split("\\s")) {
+                    out.collect(Tuple2.of(word, 1));
+                }
+            }
+        }).keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .sum(1);
+
+        windowCounts.print().setParallelism(1);
+        environment.execute("ChengJian WordCount");
+    }
+}
